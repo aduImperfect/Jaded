46c9de27e7149cb304d2cb0e4d1824f54cc7090c
DESKTOP-NAD15B0\Aditya PC <adu101992>
2018-05-13 12:31:59 +0530

    - Adding up a Unity Game Project. - New TestScene1 added. - New PlayerCube with movement code for X and Z movement added. - Jump code in progress. Currently translates in Y axis upwards if trying to jump. - Using the axis input for movement hence (PS4, XBX...) controllers inherently supported and working.

---
+++ Jaded Game Project/Jaded Unity Project/Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * FILE: Controller.cs
+ * CREATED ON: 13TH May 2018
+ * CREATED BY: Aditya Subramanian
+ * EDITED BY: Aditya Subramanian, 
+ * PURPOSE: Creating a player controller behavior.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float fXMovement;
+    public float fZMovement;
+    public float fYJump;
+
+    public float SpeedMultiplier;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        fXMovement = Input.GetAxis("Horizontal") * Time.deltaTime * SpeedMultiplier;
+        fZMovement = Input.GetAxis("Vertical") * Time.deltaTime * SpeedMultiplier;
+
+        fYJump = Input.GetAxis("Jump") * Time.deltaTime * SpeedMultiplier;
+
+        this.transform.Translate(fXMovement, fYJump, fZMovement);
+	}
+}
