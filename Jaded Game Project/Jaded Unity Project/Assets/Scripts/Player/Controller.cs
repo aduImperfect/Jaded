4e089643501d18e82ae0483d45586bd086cc7ffc
DESKTOP-NAD15B0\Aditya PC <adu101992>
2018-05-13 19:29:55 +0530

    - Added death object. - Added colliders to objects. - OnCollisionEnter() and OnCollisionStay() added to the PlayerController. - Jump getting triggered but need to add the code for actual jumping. - Need to enabled back the ground collider when no object is touching it. - Need to add ignore collision for same collider type objects for less collision overall to happen in the game world.

--- Jaded Game Project/Jaded Unity Project/Assets/Scripts/Player/Controller.cs
+++ Jaded Game Project/Jaded Unity Project/Assets/Scripts/Player/Controller.cs
@@ -15,21 +15,82 @@ public class Controller : MonoBehaviour
     public float fZMovement;
     public float fYJump;
 
-    public float SpeedMultiplier;
+    public bool bIsJumpingUp;
+    public bool bIsFallingDown;
+
+    public float fMovementSpeed;
+    public float fJumpSpeed;
+
+    public Vector3 v3JumpLimiterPosition;
+
+    public Vector3 v3PlayerPosition;
 
     // Use this for initialization
     void Start ()
     {
+        fYJump = 0.0f;
+        bIsJumpingUp = false;
+        bIsFallingDown = false;
+        v3PlayerPosition = this.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        fXMovement = Input.GetAxis("Horizontal") * Time.deltaTime * SpeedMultiplier;
-        fZMovement = Input.GetAxis("Vertical") * Time.deltaTime * SpeedMultiplier;
+        fXMovement = Input.GetAxis("Horizontal") * Time.deltaTime * fMovementSpeed;
+        fZMovement = Input.GetAxis("Vertical") * Time.deltaTime * fMovementSpeed;
 
-        fYJump = Input.GetAxis("Jump") * Time.deltaTime * SpeedMultiplier;
+        if(bIsJumpingUp)
+        {
+        }
+
+        if(bIsFallingDown)
+        {
+            Debug.Log("Is Falling Down");
+        }
 
         this.transform.Translate(fXMovement, fYJump, fZMovement);
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Death")
+        {
+            Debug.Log("Player Died!");
+            this.transform.position = v3PlayerPosition;
+            bIsFallingDown = false;
+            bIsJumpingUp = false;
+            return;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider == null)
+        {
+            if (!bIsJumpingUp)
+            {
+                //In free fall!
+                bIsFallingDown = true;
+            }
+            else
+            {
+                //In jumping mode!
+                bIsFallingDown = false;
+            }
+            return;
+        }
+
+        if (collision.collider.tag == "Ground")
+        {
+            if(Input.GetAxis("Jump") != 0.0f)
+            {
+                Debug.Log("Is Jumping!!");
+                collision.collider.enabled = false;
+                bIsJumpingUp = true;
+            }
+
+            return;
+        }
+    }
 }
