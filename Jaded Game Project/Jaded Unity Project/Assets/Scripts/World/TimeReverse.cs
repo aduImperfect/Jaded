67575aefc21bce97306be0f3f2c17117b4cea0e1
DESKTOP-NAD15B0\Aditya PC <adu101992>
2018-05-27 21:35:19 +0530

    Adding up time reverse and a failed try at adding rigidbody to other elements in the scene. Reverted back. Time reverse works fine. Although, jumping seems to only happen when movement occurs!?

---
+++ Jaded Game Project/Jaded Unity Project/Assets/Scripts/World/TimeReverse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeReverse : MonoBehaviour {
+
+    Stack<Vector3> transformStack = new Stack<Vector3>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Fire1") == true)
+        {
+            if(transformStack.Count != 0)
+            {
+                this.transform.position = transformStack.Pop();
+                return;
+            }
+        }
+
+        transformStack.Push(this.transform.position);
+    }
+}
